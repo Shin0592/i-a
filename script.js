5ec4cc05fb7c5f12da03f6b7dca1b771e08f74df
Matheus0592 <146214732+Shin0592@users.noreply.github.com>
2025-06-27 10:44:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual seria o melhor uso desta nova tecnologia na sua opnião?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso Ser usado ao máximo possível!",
+                afirmacao: "Facilitar a vida humana é o objetivo de criar novas tecnologias. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Isso deveria ser limitado!",
+                afirmacao: "Uma tecnologia com essas habilidades podem ser usadas do jeito errado por pessoas mal-intencionadas."
             }
         ]
     },
